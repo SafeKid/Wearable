16e5652e8528cdf5c7b6fa9d29df1d83f36df007
Shanilka-Navagamuwa <40514542+Shanilka-Navagamuwa@users.noreply.github.com>
2020-07-13 16:29:19 +0530

    Algorithm test

---
+++ Heart rate detection algorithm.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main(){
+	int hr[20]
+	for(int i=1;i>=40,i++){
+		
+	}
+}
+
+int heartRateCheck(int hr[20]){
+	int counter;
+	int panic;
+	x++;
+	if (counter>=20 && hr[19]-hr[0]>=40)
+	panic=1;
+}
